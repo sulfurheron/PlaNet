c24eb51030e05d92562e256d48f34f54e0f72205
Junjiang Li <junjiang.li@ocado.com>
2022-05-31 16:02:27 -0400

    Set up buffer

--- core/buffer.py
+++ core/buffer.py
@@ -1,2 +1,36 @@
+import torch
+import numpy as np
+
+from torch import Tensor
+from typing import List, Union
+from numpy import ndarray
+
+
 class Buffer:
     """Buffer holding prior experiences."""
+
+    def __init__(self) -> None:
+        """Initializes an empty buffer."""
+        self.buffer = []
+
+    def sample_sequence(self, batch_size: int) -> Union[
+            List[Tensor], List[Tensor], List[Tensor]]:
+        """Samples a consecutive sequence of observations."""
+        idxes = np.random.choice(len(self.buffer), batch_size, False)
+        obss, acts, rews = [], [], []
+
+        for idx in idxes:
+            obs, act, rew = self.buffer[idx]
+            obss.append(obs)
+            acts.append(act)
+            rews.append(rew)
+
+    def push(
+            self, obss: List[ndarray], acts: List[ndarray], rews: List[ndarray]
+    ) -> None:
+        """Pushes a new observation onto the buffer"""
+        obss = torch.tensor(obss)
+        acts = torch.tensor(acts)
+        rews = torch.tensor(rews)
+
+        self.buffer.append((obss, acts, rews))
